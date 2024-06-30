abf5c0ee9330aaa5e5509df2d823ab8bde2dbb1f
TAbdiukov <TAbdiukov@gmail.com>
2024-06-30 17:43:06 +0200

    Fix Python whitespaces

--- gen_html_toc.py
+++ gen_html_toc.py
@@ -24,24 +24,24 @@ def showHelp():
 	print("python "+PROGRAM_NAME_SHORT() +".py C://HTML/*.* False")
 	print("python "+PROGRAM_NAME_SHORT() +".py *.htm*")
 	print("python "+PROGRAM_NAME_SHORT() +".py *1*.7z")
-	
+
 def kwikHash(txt):
 	return toHexCustom(zlib.adler32(txt.encode('utf-8')))
 
-def toHexCustom(dec): 
-	return str(hex(dec).split('x')[-1])	
+def toHexCustom(dec):
+	return str(hex(dec).split('x')[-1])
 
-	
+
 # https://stackoverflow.com/a/715468
 def str2bool(v):
 	return str(v).lower() in ("yes", "true", "t", "1")
-	
+
 def path_process(p):
 	#OOP copy
 	buf = Path(p)
 	if(os.path.isdir(buf)):
 		buf = buf.joinpath("*")
-	
+
 	return str(buf)
 
 
@@ -52,20 +52,20 @@ def main():
 	else:
 		path = Path(str(sys.argv[1])).absolute()
 		r = DEF_RECURSIVE
-		
+
 		if(argc == 2):
 			r = str2bool(sys.argv[2])
 			print("Recursive flag input found! Recursive set to: "+str(r).upper())
-		
+
 		# init
 		processed = path_process(path) # any
 		projname = path.with_name("foo").parts[-2]
-		
-		
+
+
 		listing = sorted(glob.glob(processed, recursive = r),key = lambda x: [int(k) if k.isdigit() else k for k in re.split('([0-9]+)', Path(x).stem)])
-		
+
 		construct = HEADER.format(projname,projname) +'\n'
-		
+
 		# begin building pieces
 		cnt = 0
 		for i in listing:
@@ -73,19 +73,19 @@ def main():
 				k = os.path.basename(i)
 				construct += '        <a href="{}">{}</a><br/>\n'.format(k,k)
 				cnt += 1
-				
+
 		print("{} files processed.".format(cnt))
-		
+
 		construct += FOOTER + "\n"
-		
+
 		fname = projname+"_toc.htm"
 		print("Data is saved to: "+fname)
 		output_name = str(path.with_name(fname))
-		
+
 		# output
 		fp = open(output_name, "w")
 		fp.write(construct)
 		fp.close()
-		
+
 if __name__ == '__main__':
 	main()
--- img2.py
+++ img2.py
@@ -5,7 +5,7 @@ from pathlib import Path #PY3
 try:
 	import keyboard
 	import pyperclip
-	
+
 	# not-really-an-import but ok
 	import img2pdf
 except ImportError:
@@ -28,8 +28,8 @@ def PROGRAM_NAME(): return "img2"
 DELIMITER = " " #WHITESPACE
 C34 = chr(34)
 
-def toHexCustom(dec): 
-	return str(hex(dec).split('x')[-1])	
+def toHexCustom(dec):
+	return str(hex(dec).split('x')[-1])
 
 def getModulePath(s):
 	query = str(eval(s+".__file__")).strip()
@@ -37,10 +37,10 @@ def getModulePath(s):
 	if(query_ext=="py"):
 		query = "python "+query
 	return query
-	
+
 def kwikHash(txt):
 	return toHexCustom(zlib.adler32(txt.encode('utf-8')))
-	
+
 def showHelp():
 	print(PROGRAM_NAME()+" v"+VERSION()+" - img2pdf's wrapper on PY3 to help save to PDF losslessly")
 	print("USAGE:")
@@ -51,9 +51,9 @@ def path_process(p):
 	buf = Path(p)
 	if(os.path.isdir(buf)):
 		buf = buf.joinpath("*")
-	
+
 	return str(buf)
-	
+
 def list_all_do():
 	for i, k in DO.items():
 		print(f"\tDO - \t{i}: {k}")
@@ -66,7 +66,7 @@ def gen_command():
 	ret = s
 	s_list = s.split(" ")
 	s_list_first = s_list[0]
-	
+
 	# is syntax like 'python3 blabla'?
 	if(("python" in s_list_first.lower()) and (len(s_list) > 1)):
 		s_potentially_quotationmarks = s_list[1][0] + s_list[-1][-1]
@@ -78,10 +78,10 @@ def gen_command():
 			# 2. Recompile the split with for-loop
 			# I think both are equally valid, since we splice by ' ' and not '\w'
 			ret = s_list_first+' "'+s[len(s_list_first)+1:]+'"'
-			
-		
+
+
 	return ret
-	
+
 
 def main():
 	argc = len(sys.argv) - 1
@@ -89,69 +89,69 @@ def main():
 		#input
 		path = Path(str(sys.argv[1]))
 		dpi_pure = str(sys.argv[2])
-	
+
 		# error chk
 		try:
 			dpi_float = float(dpi_pure)
 			assert(dpi_float > 0)
 		except:
 			raise ValueError("Invalid DPI")
-					
+
 		#init
 		processed = path_process(path) # any
 		listing = glob.glob(processed, recursive=True)
 		construct = ""
-		
+
 		# Informational
 		print("Mode of operation,")
 		list_all_do()
 		# begin building pieces
-		
+
 		command = gen_command()
 		#help = eval("img2pdf -h")
 		dpi_arg = dpi_pure+"dpix"+dpi_pure+"dpi"
-		
+
 		## input list process
 		input_list = ""
 		for i in listing:
 			if(os.path.isfile(i)):
 				input_list += C34+i+C34 +DELIMITER
-		
+
 		input_list = input_list[:-1] #remove trailing whitespace
-		
+
 		input_list_len = len(input_list)
 		if(len(input_list)<= 0):
-			raise ValueError("Nothing to process")			
-		
+			raise ValueError("Nothing to process")
+
 		##Compute quick&dirty list hash
 		input_list_hash = kwikHash(input_list)
 		output_pdf_name = C34+str(path.with_name(input_list_hash+".pdf"))+C34
-		
+
 		# put pieces together
 		#rearranged for further convenience
-		construct = command+" "+input_list+" -o "+output_pdf_name+" -s "+dpi_arg 
-		
+		construct = command+" "+input_list+" -o "+output_pdf_name+" -s "+dpi_arg
+
 		#output:
-		
+
 		#console
 		if(DO["PREENTER"]):
 			keyboard.write(construct)#, delay=0.01)
-		
+
 		#file
 		if(DO["FILE"]):
 			fname = input_list_hash+".txt"
 			f = open(fname, "w+")
 			f.write(construct)
 			f.close()
-		
+
 		if(DO["CLIPBOARD"]):
 			#clipboard [disabled]
 			pyperclip.copy(construct)
-		
+
 		if(DO["EXEC"]):
 			# execute [disabled]
 			exec(construct)
-		
+
 		# print
 		print("done ("+str(len(construct))+")")
 		if(DO["FILE"]): print(fname)
